9f48c716f81339b77bc2a3fbc7ed7126dc87810a
rutulshah1710 <rutulshah1710@gmail.com>
2017-01-27 17:45:41 -0500

    added a comment to class header

--- comp1004-Lesson1-Part2/Program.cs
+++ comp1004-Lesson1-Part2/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace comp1004_Lesson1_Part2
 {
+    /// <summary>
+    /// Driver class for our console aplication
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
